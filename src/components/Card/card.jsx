0284dcaed021351389f560032f09deb451d9caa7
Huiyi Wang <wanghy815@gmail.com>
2024-09-22 16:36:06 -0400

    update card, map styling. Add new select 2 hobbies feature

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import maplibregl from 'maplibre-gl';
+import pin from '../../assets/logo/snoopy-pin.png';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './card.scss';
 
@@ -12,22 +13,54 @@ const Card = ({ name, address, description, longitude, latitude }) => {
       zoom: 15,
     });
 
-    new maplibregl.Marker()
-      .setLngLat([longitude, latitude])
-      .addTo(map);
+    map.on('load', () => {
+      const img = new Image(300, 300);
+      img.src = pin;
 
-    return () => map.remove();
+      img.onload = () => {
+        map.addImage('snoopypin', img);
+
+        map.addSource('point', {
+          'type': 'geojson',
+          'data': {
+            'type': 'FeatureCollection',
+            'features': [
+              {
+                'type': 'Feature',
+                'geometry': {
+                  'type': 'Point',
+                  'coordinates': [longitude, latitude],
+                },
+              },
+            ],
+          },
+        });
+
+        map.addLayer({
+          'id': 'points',
+          'type': 'symbol',
+          'source': 'point',
+          'layout': {
+            'icon-image': 'snoopypin',
+            'icon-size': 0.25,
+          },
+        });
+      };
+    });
+
+    return () => {
+      if (map) {
+        map.remove();
+      }
+    };
   }, [longitude, latitude, name]);
 
   return (
     <div className="card">
-      <h3 className='card__title'>{name}</h3>
-      <p className='card__address'>Address: {address}</p>
-      <p className='card__description'>Description: {description}</p>
-      
       <div id={`map-${name}`} className="map-container"></div>
-
-      <p className='card__location'>Location: {longitude}, {latitude}</p>
+      <h3 className='card__title'>{name}</h3>
+      <h3 className='card__address'>{address}</h3>
+      <p className='card__description'>{description}</p>
     </div>
   );
 };
